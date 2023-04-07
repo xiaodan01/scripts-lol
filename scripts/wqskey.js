34a908cf0ee0c073b8d26f6998f6c34b1c99e4f1
xiaodan01 <65934452+xiaodan01@users.noreply.github.com>
2023-04-07 14:42:18 +0800

    Update wqskey.js

--- scripts/wqskey.js
+++ scripts/wqskey.js
@@ -1,6 +1,6 @@
 //微信打开获取 https://wqsh.jd.com/promote/201801/bean/index.html?PTAG=17053.1.1&utm_source=weixin#/formal
 //匹配wqskey
-var cookieReg = /^.*?\bwq_skey=([a-fA-F0-9]+).*$/;
+var cookieReg = /^.*?\wq_skey=([a-fA-F0-9]+).*$/;
 //获取cookie
 var cookie = $request.headers.Cookie;
 //获取匹配结果
