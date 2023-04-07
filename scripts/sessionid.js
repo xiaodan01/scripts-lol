b507b03a94d2318a427076cc3459d957a51f2516
xiaodan01 <65934452+xiaodan01@users.noreply.github.com>
2023-04-07 14:12:12 +0800

    Update sessionid.js

--- scripts/sessionid.js
+++ scripts/sessionid.js
@@ -7,14 +7,25 @@ https://api5-(.+).pipix.com/luckycat/bds/v1/task/done/excitation_ad? url script-
 api5-(.+).pipix.com
 
 */
+//匹配cookie中的sessionid
 var cookieReg = /^.*?\bsessionid=([a-fA-F0-9]+).*$/;
+//获取cookie
 var cookie = $request.headers.Cookie;
+//获取匹配结果
 var sessionid = cookie.match(cookieReg)[1];
 var url = $request.url;
+//获取Token
 var x-Tt-Token = $request.header.x-Tt-Token;
+//获取Khronos
 var X-Khronos = $request.header.x-Khronos;
+//获取Gorgon
 var X-Gorgon = $request.header.x-Gorgon;
+//组合结果
+var ck = "["+url+"#"+sessionid+"#"+x-Tt-Token+"#"+X-Khronos+"#"+X-Gorgon+"]";
 
-console.log(sessionid);
-$notify("获取到了亮总ppx", "", url+"#"+sessionid+"#"+x-Tt-Token+"#"+X-Khronos+"#"+X-Gorgon);
+console.log(ck);
+//复制ck到剪切板
+$clipboard.text = ck;
+//通知
+$notify("获取到了亮总ppx", "", ck);
    $done();
